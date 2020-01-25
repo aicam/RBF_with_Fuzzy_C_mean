42ab5cb3188da0af6855ea714a0a3b1e385bb67a
ali <a_icam@yahoo.com>
2020-01-25 18:03:59 +0330

    full

--- FCM.py
+++ FCM.py
@@ -43,31 +43,32 @@ def fcm(data,Y, classes_count=2, n_clusters=1, landa=.1, n_init=30, m=2, max_ite
     plt.plot(data[:, 0], data[:, 1], 'go', min_centers[:, 0], min_centers[:, 1], 'bs')
     plt.show()
     C = []
-    for i in range(len(min_centers)):
-        C_numerator = 0
-        C_denominator = 0
-        for j in range(len(data)):
-            C_numerator += (um[i][j] ** m)
-        for j in range(len(data)):
-
-            C_denominator += ((um[i][j] ** m) * np.matmul(np.transpose(np.array(data[j]) - np.array(min_centers[i])),
-                                                          np.array(np.array(data[j]) - np.array(min_centers[i]))))
-        C.append(C_numerator / C_denominator)
+    C = calculate_covariance_mat(data,min_centers,um,m)
+    # for i in range(len(min_centers)):
+    #     C_numerator = 0
+    #     C_denominator = 0
+    #     for j in range(len(data)):
+    #         C_numerator += (um[i][j] ** m)
+    #     for j in range(len(data)):
+    #
+    #         C_denominator += ((um[i][j] ** m) * np.matmul(np.transpose(np.array(data[j]) - np.array(min_centers[i])),
+    #                                                       np.array(np.array(data[j]) - np.array(min_centers[i]))))
+    #     C.append(C_numerator / C_denominator)
     G = np.zeros([len(data), len(min_centers)])
 
     for k in range(len(data)):
         for i in range(len(min_centers)):
             G[k][i] += np.exp(-landa * np.matmul(
-                np.matmul(np.transpose(np.array(data[k]) - np.array(min_centers[i])), generate_C_inverse(data[k], min_centers[i])),
+                np.matmul(np.transpose(np.array(data[k]) - np.array(min_centers[i])), np.linalg.inv(C[i])),
                 np.array(np.array(data[k]) - np.array(min_centers[i]))))
     try:
         V = np.matmul(np.transpose(G),G)
-        print(V)
+        # print(V)
         W = np.matmul(np.matmul(np.linalg.inv(np.matmul(np.transpose(G),G)),np.transpose(G)),generate_Y(len(data),Y,classes_count))
     except np.linalg.LinAlgError:
         W = np.matmul(np.matmul(np.random.rand([len(min_centers),len(min_centers)]), np.transpose(G)), generate_Y(len(data), Y,classes_count))
     y_hat = np.argmax(np.matmul(G,W), axis=1)
-    # print(W)
+    print(W)
     return min_centers
 
 def generate_Y(x_d, y_vector, classes_count):
@@ -78,15 +79,19 @@ def generate_Y(x_d, y_vector, classes_count):
         Y.append(y_i)
     return np.array(Y).reshape([x_d, classes_count])
 
-def generate_C_inverse(x, v):
-    ci = np.zeros([len(v), len(v)])
-    for i in range(len(v)):
-        for j in range(len(x)):
-            ci[i][j] += v[i] * x[j]
-    try:
-        ci_inverse = np.linalg.inv(ci)
-    except np.linalg.LinAlgError:
-        ci_inverse = np.zeros([len(v),len(v)])
-        for i in range(len(v)):
-            ci_inverse[i][i] = 1
-    return ci_inverse
+def calculate_covariance_mat(data, centers, U, m):
+    C = []
+    centers = np.array(centers)
+    for i in range(len(centers)):
+        shape = (2,2)
+        divided_sum = np.zeros(shape)
+        divisor_sum = .0
+        for k in range(len(data)):
+            divided_sum += np.power(U[i][k],m)* np.multiply(
+                np.array([data[k] - centers[i]]), np.transpose(
+                    np.array([data[k] - centers[i]])
+                )
+            )
+            divisor_sum += np.power(U[i][k], m)
+        C.append(divided_sum/ divisor_sum)
+    return C
